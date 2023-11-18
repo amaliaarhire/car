4061e838e912e388eb37878557ba0416f8ef7201
amaliaarhire <119457241+amaliaarhire@users.noreply.github.com>
2023-11-18 12:56:33 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Ford = new Car("Ford", "white", 2021, 150);
+            Car Opel = new Car("Mercedes", "blue", 2022, 180);
+            Car Volvo = new Car("XC_90");
+            //Ford.name="Ford";
+            //Ford.year=2021;
+            //Ford.color="white";
+            //Ford.maxSpeed=150;
+
+            //Opel.name = "Mercedes";
+            //Opel.year = 2022;
+            //Opel.color = "blue";
+            //Opel.maxSpeed = 180;
+
+            Console.WriteLine(Ford.name);
+            Console.WriteLine(Ford.color);
+            Console.WriteLine(Ford.year);
+            Ford.fullThrottle();
+
+            Console.WriteLine(Opel.name);
+            Console.WriteLine(Opel.color);
+            Console.WriteLine(Opel.year);
+            Opel.fullThrottle();
+            Console.ReadKey();
+        }
+    }
+}
